db032f6a2215b6c2fa7a8eb5766519f34e999d03
Nguyen Cong Duy <ncongduy@gmail.com>
2021-04-10 16:24:37 +0300

    part 3 - Ravenous

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,32 +1,43 @@
-import React from 'react';
-import './App.css';
-import BusinessList from '../Business/BusinessList';
-import SearchBar from '../SearchBar/SearchBar';
+import React from "react";
+import "./App.css";
+import BusinessList from "../Business/BusinessList";
+import SearchBar from "../SearchBar/SearchBar";
 
 const bussiness = {
-	imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-	name: 'MarginOtto Pizzeria',
-	address: '1010 Paddington Way',
-	city: 'Flavortown',
-	state: 'NY',
-	zipCode: '10101',
-	category: 'Italian',
-	rating: 4.5,
-	reviewCount: 90
+  imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
+  name: "MarginOtto Pizzeria",
+  address: "1010 Paddington Way",
+  city: "Flavortown",
+  state: "NY",
+  zipCode: "10101",
+  category: "Italian",
+  rating: 4.5,
+  reviewCount: 90,
 };
 
-const businesses = [bussiness,bussiness,bussiness,bussiness,bussiness,bussiness];
+const businesses = [
+  bussiness,
+  bussiness,
+  bussiness,
+  bussiness,
+  bussiness,
+  bussiness,
+];
 
 class App extends React.Component {
-	render() {
-		return (
-			<div className="App">
-				<h1>ravenous</h1>
-				<SearchBar />
-				<BusinessList businesses={businesses} /> 
-			</div>
-		);
-	}
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={businesses} />
+      </div>
+    );
+  }
 }
 
 export default App;
