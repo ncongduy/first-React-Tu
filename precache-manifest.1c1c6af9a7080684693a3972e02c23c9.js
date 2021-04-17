674bf229b15507d4097c4ce2a8e86fae6a0c09f4
Nguyen Cong Duy <ncongduy@gmail.com>
2021-04-17 20:22:26 +0300

    Updates

---
+++ precache-manifest.1c1c6af9a7080684693a3972e02c23c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dae12ad496439ea3862db53abae7911",
+    "url": "/first-React-Tu/index.html"
+  },
+  {
+    "revision": "7a9e43a9275abbae23fe",
+    "url": "/first-React-Tu/static/css/main.1d7bc1a6.chunk.css"
+  },
+  {
+    "revision": "bde4f3c2f7af6bad700f",
+    "url": "/first-React-Tu/static/js/2.14a4c7d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-React-Tu/static/js/2.14a4c7d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a9e43a9275abbae23fe",
+    "url": "/first-React-Tu/static/js/main.968d46da.chunk.js"
+  },
+  {
+    "revision": "2594790a9db8ad58db72",
+    "url": "/first-React-Tu/static/js/runtime-main.c1d8e090.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/first-React-Tu/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "082e88ec250194e6416adc8772adea4e",
+    "url": "/first-React-Tu/static/media/background_search_mobile.082e88ec.jpg"
+  }
+]);
